99139492a5328be1d90aca936b5923a5438c018d
jgluhov <jgluhov@gmail.com>
2018-01-21 18:19:37 +0300

    refactoring

---
+++ src/rx.ts
@@ -0,0 +1,15 @@
+export { Observable } from 'rxjs/observable';
+export { defer } from 'rxjs/observable/defer';
+export { from } from 'rxjs/observable/from';
+export { timer } from 'rxjs/observable/timer';
+export { of } from 'rxjs/observable/of';
+export { interval } from 'rxjs/observable/interval';
+export { map } from 'rxjs/operators/map';
+export { tap } from 'rxjs/operators/tap';
+export { concat } from 'rxjs/operators/concat';
+export { mergeMap } from 'rxjs/operators/mergeMap';
+export { switchMap } from 'rxjs/operators/switchMap';
+export { concatMap } from 'rxjs/operators/concatMap';
+export { pairwise } from 'rxjs/operators/pairwise';
+export { takeWhile } from 'rxjs/operators/takeWhile'
+export { animationFrame } from 'rxjs/scheduler/animationFrame';
